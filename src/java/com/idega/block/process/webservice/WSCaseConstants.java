7fc0fabc0f877ef7e99440e04c4df5e30eefb7dd
Grimur Jonsson <gimmi@idega.com>
2006-01-06 01:05:45 +0000

    constants for the webservice

---
+++ src/java/com/idega/block/process/webservice/WSCaseConstants.java
@@ -0,0 +1,19 @@
+/*
+ * $Id: WSCaseConstants.java,v 1.1 2006/01/06 01:05:45 gimmi Exp $
+ * Created on Jan 5, 2006
+ *
+ * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.block.process.webservice;
+
+
+public class WSCaseConstants {
+	public static final String STATUS_IN_PROCESS 	= "IN PROCESS";
+	public static final String STATUS_CLOSED 		= "CLOSED";
+	public static final String STATUS_PENDING 		= "PENDING";
+	public static final String STATUS_ARCHIVED 		= "ARCHIVED";
+	public static final String STATUS_LOCKED 		= "LOCKED";
+}
