d1fc581b089531a370f1c5ed21094e9c4b891765
Grimur Jonsson <gimmi@idega.com>
2006-01-09 10:36:25 +0000

    some system out thingies

--- src/java/com/idega/block/process/business/CaseBusinessBean.java
+++ src/java/com/idega/block/process/business/CaseBusinessBean.java
@@ -642,6 +642,7 @@ public class CaseBusinessBean extends IBOServiceBean implements CaseBusiness {
 	public Case createOrUpdateCase(_case wsCase) throws Exception {
 		
 		String status = wsCase.getStatus();
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] status = "+status);
 		if (status.equals(WSCaseConstants.STATUS_ARCHIVED)) {
 			status = getCaseHome().getCaseStatusArchived();
 		} else if (status.equals(WSCaseConstants.STATUS_CLOSED)) {
@@ -653,16 +654,19 @@ public class CaseBusinessBean extends IBOServiceBean implements CaseBusiness {
 		} else if (status.equals(WSCaseConstants.STATUS_PENDING)) {
 			status = getCaseHome().getCaseStatusPending();
 		} 
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] status = "+status);
 		
 		CaseStatus newCaseStatus = getCaseStatus(status);
 		
 		String id = wsCase.getId();
 		Case theCase = null;
 		if (id == null || "-1".equals(id)) {
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] creating");
 			theCase = getCaseHome().create();
 			theCase.setCreated(new IWTimestamp(wsCase.getCreated()).getTimestamp());
 			theCase.setCaseStatus(newCaseStatus);
 		} else {
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] updating");
 			theCase = getCaseHome().findByPrimaryKey(new Integer(id));
 			changeCaseStatus(theCase, newCaseStatus, null);
 		}
@@ -670,17 +674,25 @@ public class CaseBusinessBean extends IBOServiceBean implements CaseBusiness {
 		theCase.setExternalId(wsCase.getExternal_case_id());
 		theCase.setBody(wsCase.getBody());
 		theCase.setSubject(wsCase.getSubject());
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] Code = "+wsCase.getCode());
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] extCase = "+wsCase.getExternal_case_id());
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] Body = "+wsCase.getBody());
+		System.out.println("[CaseBusiness : craeteOrUpdateCase] Subject = "+wsCase.getSubject());
 
 		try {
 			User owner = getUserHome().findByPersonalID(wsCase.getOwner().getSocialsecurity());
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] owner = "+owner.getPersonalID());
 			theCase.setOwner(owner);
 		} catch (FinderException f) {
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] no owner");
 			f.printStackTrace();
 		}
 		try {
 			User handler = getUserHome().findByPersonalID(wsCase.getHandler().getSocialsecurity());
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] handler = "+handler.getPersonalID());
 			theCase.setExternalHandler(handler);
 		} catch (FinderException f) {
+			System.out.println("[CaseBusiness : craeteOrUpdateCase] no handler");
 			f.printStackTrace();
 		}
 		theCase.store();
