84064f0fdd0fcb872f7a7d0a85c2045805214722
Eirikur Hrafnsson <eiki@idega.com>
2007-01-30 07:07:19 +0000

    Added methods in CaseBusiness to get a users preferred IWRB by his preferred locale and deprecated getLocalizedString but improved it though. I did this because when the system sends a citizen messages they should be sent in the users preferred language!

--- src/java/com/idega/block/process/business/CaseBusinessHomeImpl.java
+++ src/java/com/idega/block/process/business/CaseBusinessHomeImpl.java
@@ -1,31 +1,16 @@
-/*
- * $Id: CaseBusinessHomeImpl.java,v 1.14 2006/03/30 11:19:50 thomas Exp $
- * Created on Mar 27, 2006
- *
- * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
- */
 package com.idega.block.process.business;
+
+
+import javax.ejb.CreateException;
 
 import com.idega.business.IBOHomeImpl;
 
-
-/**
- * 
- *  Last modified: $Date: 2006/03/30 11:19:50 $ by $Author: thomas $
- * 
- * @author <a href="mailto:thomas@idega.com">thomas</a>
- * @version $Revision: 1.14 $
- */
 public class CaseBusinessHomeImpl extends IBOHomeImpl implements CaseBusinessHome {
-
-	protected Class getBeanInterfaceClass() {
+	public Class getBeanInterfaceClass() {
 		return CaseBusiness.class;
 	}
 
-	public CaseBusiness create() throws javax.ejb.CreateException {
+	public CaseBusiness create() throws CreateException {
 		return (CaseBusiness) super.createIBO();
 	}
 }
