250e61713107e56413d6cf0dd62122af4fa4e829
Valdas Zemaitis <valdas@idega.com>
2008-06-02 08:58:11 +0000

    *** empty log message ***

--- src/java/com/idega/block/process/util/CaseComparator.java
+++ src/java/com/idega/block/process/util/CaseComparator.java
@@ -1,21 +1,15 @@
 package com.idega.block.process.util;
 
+import java.sql.Timestamp;
 import java.util.Comparator;
 
 import com.idega.block.process.data.Case;
-import com.idega.util.IWTimestamp;
 
 public class CaseComparator implements Comparator<Case> {
 
 	public int compare(Case case1, Case case2) {
-		IWTimestamp time1 = null;
-		IWTimestamp time2 = null;
-		try {
-			time1 = new IWTimestamp(case1.getCreated());
-			time2 = new IWTimestamp(case2.getCreated());
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		Timestamp time1 = case1.getCreated();
+		Timestamp time2 = case2.getCreated();
 		
 		if (time1 == null || time2 == null) {
 			return 0;
