941c749bb99a0fd1ec3a89231a57063025f22ecc
Valdas Zemaitis <valdas@idega.com>
2009-06-17 12:34:43 +0000

    Now can compare by subjects

--- src/java/com/idega/block/process/presentation/beans/CasePresentationComparator.java
+++ src/java/com/idega/block/process/presentation/beans/CasePresentationComparator.java
@@ -1,11 +1,27 @@
 package com.idega.block.process.presentation.beans;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
 public class CasePresentationComparator implements Comparator<CasePresentation> {
 
+	private Collator collator;
+	
+	public CasePresentationComparator() {
+		super();
+	}
+	
+	public CasePresentationComparator(Locale locale) {
+		this();
+		
+		if (locale != null) {
+			collator = Collator.getInstance(locale);
+		}
+	}
+	
 	public int compare(CasePresentation case1, CasePresentation case2) {
-		return -(case1.getCreated().compareTo(case2.getCreated()));
+		return collator == null ? -(case1.getCreated().compareTo(case2.getCreated())) : collator.compare(case1.getSubject(), case2.getSubject());
 	}
 
 }
