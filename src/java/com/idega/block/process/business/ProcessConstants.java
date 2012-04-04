1fce2364aa4bd8007a3bf5e4d4b116afa54cf925
Valdas Žemaitis <valdas@idega.com>
2012-04-04 11:08:22 +0300

    New constants

--- src/java/com/idega/block/process/business/ProcessConstants.java
+++ src/java/com/idega/block/process/business/ProcessConstants.java
@@ -22,38 +22,41 @@ import com.idega.user.data.User;
 
 /**
  * Last modified: $Date: 2009/03/09 15:59:24 $ by $Author: valdas $
- * 
+ *
  * @author <a href="mailto:laddi@idega.com">laddi</a>
  * @version $Revision: 1.2 $
  */
 public final class ProcessConstants implements Singleton {
-	
+
 	public static final String	CASE_IDENTIFIER = "string_caseIdentifier",
-								CASE_DESCRIPTION = "string_caseDescription";
+								CASE_DESCRIPTION = "string_caseDescription",
+								FINANCING_OF_THE_TASKS = "objlist_ownerFinanceOfTasks",
+								BOARD_FINANCING_SUGGESTION = "string_ownerGradeComment",
+								BOARD_FINANCING_DECISION = "string_ownerGrantAmauntValue";
 
 	private ProcessConstants() {}
-	
+
 	public static final String	IW_BUNDLE_IDENTIFIER = "com.idega.block.process",
 								EGOV_NEST_CASES_CACHE_KEY = "egovNestCasesCacheKey",
 								ACTIVE_PROCESS_DEFINITION = "active_process_definition_id",
 								METADATA_KEY_URL="CASE_URL",
 								CASE_LIST_TYPE_SEARCH_RESULTS = "searchResults",
 								HANDLER_IDENTIFIER = "handlerUserId";
-	
+
 	public static final List<CasesListColumn> getCasesListMainColumns(IWResourceBundle iwrb) {
 		CasesListColumn[] columns = new CasesListColumn[6];
 		Random generator = new Random();
-		
+
 		columns[0] = new CasesListColumn(iwrb.getLocalizedString("number", "Number"), "number", String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
 		columns[1] = new CasesListColumn(iwrb.getLocalizedString("status", "Status"), "string", String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
 		columns[2] = new CasesListColumn(Text.NON_BREAKING_SPACE, null, String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
 		columns[3] = new CasesListColumn(iwrb.getLocalizedString("sender", "Sender"), "string", String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
 		columns[4] = new CasesListColumn(iwrb.getLocalizedString("handler", "Handler"), "string", String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
 		columns[5] = new CasesListColumn(iwrb.getLocalizedString("date", "Date"), "date", String.valueOf(generator.nextInt(Integer.MAX_VALUE)));
-		
+
 		return Collections.unmodifiableList(Arrays.asList(columns));
 	}
-	
+
 	public static final String getKeyForCasesColumnsAttribute(IWContext iwc) {
 		if (iwc == null) {
 			throw new NullPointerException(ProcessConstants.class.getName() + ": IWContext is null");
