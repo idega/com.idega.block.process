8ba1510510e4df5a14ccc31f59e3b81f2dc1cecc
Valdas Žemaitis <valdas@idega.com>
2013-12-04 12:45:45 +0200

    New method to get localized statuses

--- src/java/com/idega/block/process/presentation/beans/CasesListCustomizer.java
+++ src/java/com/idega/block/process/presentation/beans/CasesListCustomizer.java
@@ -1,18 +1,22 @@
 package com.idega.block.process.presentation.beans;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public interface CasesListCustomizer {
 
 	public List<String> getHeaders(List<String> headersKeys);
-	
+
 	public Map<String, Map<String, String>> getLabelsForHeaders(List<String> casesIds, List<String> headersKeys);
-	
+
 	/**
 	 * @param headersKeys not <code>null</code>
 	 * @return {@link Map} of (headerKey, header) or <code>null</code>.
 	 * @author <a href="mailto:martynas@idega.com">Martynas Stakė</a>
 	 */
-	public java.util.Map<String, String> getHeadersAndVariables(List<String> headersKeys);
+	public Map<String, String> getHeadersAndVariables(List<String> headersKeys);
+
+	public Map<String, String> getLocalizedStatuses(List<String> casesIds, Locale locale);
+
 }
