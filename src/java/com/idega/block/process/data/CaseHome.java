27e1db65da352aba654003971783e3db30696397
lapiukshtiss <martynas.stake@gmail.com>
2014-11-04 13:53:42 +0200

    Arraging variables

--- src/java/com/idega/block/process/data/CaseHome.java
+++ src/java/com/idega/block/process/data/CaseHome.java
@@ -231,11 +231,27 @@ public interface CaseHome extends IDOHome {
 	 */
 	public Case findCaseByUniqueId(String uniqueId) throws FinderException;
 
-	public Collection<Case> findByCriteria(String caseNumber, String description, Collection<String> owners, String[] statuses, IWTimestamp dateFrom,
-			IWTimestamp dateTo, User owner, Collection<Group> groups, boolean simpleCases) throws FinderException;
+	public Collection<Case> findByCriteria(
+			String caseNumber, 
+			String description, 
+			Collection<String> owners, 
+			String[] statuses, 
+			IWTimestamp dateFrom,
+			IWTimestamp dateTo, 
+			User owner, 
+			Collection<Group> groups, 
+			boolean simpleCases) throws FinderException;
 
-	public Collection<Integer> findIDsByCriteria(String caseNumber, String description, Collection<String> owners, String[] statuses, IWTimestamp dateFrom,
-			IWTimestamp dateTo, User owner, Collection<Group> groups, boolean simpleCases) throws FinderException;
+	public Collection<Integer> findIDsByCriteria(
+			String caseNumber, 
+			String description, 
+			Collection<String> owners, 
+			String[] statuses, 
+			IWTimestamp dateFrom,
+			IWTimestamp dateTo, 
+			User owner, 
+			Collection<Group> groups, 
+			boolean simpleCases) throws FinderException;
 
 	public abstract Collection<Case> findAllByIds(Collection<Integer> ids) throws FinderException;
 
