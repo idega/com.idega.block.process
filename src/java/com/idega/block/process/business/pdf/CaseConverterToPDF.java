82e43fe3b486832b532e4e6d814d7755bcd01a69
Valdas Žemaitis <valdas@idega.com>
2015-05-15 13:51:47 +0200

    New method

--- src/java/com/idega/block/process/business/pdf/CaseConverterToPDF.java
+++ src/java/com/idega/block/process/business/pdf/CaseConverterToPDF.java
@@ -9,6 +9,7 @@ public interface CaseConverterToPDF {
 	public List<CasePDF> getPDFsForCase(Integer caseId) throws Exception;
 
 	public List<CasePDF> getPDFsForCase(Case theCase) throws Exception;
+	public List<CasePDF> getPDFsForCase(Case theCase, boolean resetContex) throws Exception;
 
 	public List<CasePDF> getPDFsAndAttachmentsForCase(Integer caseId) throws Exception;
 
