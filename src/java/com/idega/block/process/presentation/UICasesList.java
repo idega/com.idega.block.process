443ab748b534a23814fdee9ed5cd1231af996952
Valdas Žemaitis <valdas@idega.com>
2016-09-30 11:36:24 +0300

    Removed unused import

--- src/java/com/idega/block/process/presentation/UICasesList.java
+++ src/java/com/idega/block/process/presentation/UICasesList.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.faces.component.UIComponent;
