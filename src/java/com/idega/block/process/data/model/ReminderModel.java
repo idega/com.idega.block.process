106626e6a2592d81c4a04187b2b84e5d570e046e
Valdas Žemaitis <valdas@idega.com>
2018-08-10 14:45:11 +0300

    Models for reminders and settings for case

---
+++ src/java/com/idega/block/process/data/model/ReminderModel.java
@@ -0,0 +1,22 @@
+package com.idega.block.process.data.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.idega.user.data.bean.User;
+
+public interface ReminderModel {
+
+	public void setReceivers(List<User> receivers);
+
+	public List<User> getReceivers();
+
+	public void setTimestamp(Timestamp timestamp);
+
+	public Timestamp getTimestamp();
+
+	public void setMessage(String message);
+
+	public String getMessage();
+
+}
---
+++ src/java/com/idega/block/process/data/model/SettingsModel.java
@@ -0,0 +1,26 @@
+package com.idega.block.process.data.model;
+
+import java.util.List;
+
+import com.idega.core.accesscontrol.data.bean.ICRole;
+import com.idega.user.data.bean.User;
+
+public interface SettingsModel {
+
+	public void setAutoCloseAfterInnactiveForMonths(Integer numberOfMonthsOfInnactivity);
+
+	public Integer getAutoCloseAfterInnactiveForMonths();
+
+	public void setReminders(List<ReminderModel> reminders);
+
+	public List<ReminderModel> getReminders();
+
+	public void setPredefinedListOfThirdPartiesToInvite(List<User> thirdPartiesToInvite);
+
+	public List<User> getPredefinedListOfThirdPartiesToInvite();
+
+	public void setRolesToHandle(List<ICRole> roles);
+
+	public List<ICRole> getRolesToHandle();
+
+}
