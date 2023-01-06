b6fb1b8766dae75c1c907db59d7faec110536847
Valdas Žemaitis <valdas@idega.com>
2023-01-06 13:19:05 +0200

    New flag if to add default fields to exported file

--- src/java/com/idega/block/process/presentation/beans/CasesSearchResultsHolder.java
+++ src/java/com/idega/block/process/presentation/beans/CasesSearchResultsHolder.java
@@ -21,12 +21,12 @@ public interface CasesSearchResultsHolder {
 
 	public boolean isAllDataLoaded(String id);
 
-	public boolean doExport(String id, boolean exportContacts, boolean showCompany);
+	public boolean doExport(String id, boolean exportContacts, boolean showCompany, boolean addDefaultFields);
 
-	public MemoryFileBuffer getExportedSearchResults(String id, boolean exportContacts, boolean showCompany);
+	public MemoryFileBuffer getExportedSearchResults(String id, boolean exportContacts, boolean showCompany, boolean addDefaultFields);
 	public MemoryFileBuffer getUsersExport(Collection<User> users,Locale locale,boolean showUserCompany);
 
-	public MemoryFileBuffer getExportedCases(String id, boolean exportContacts, boolean showCompany);
+	public MemoryFileBuffer getExportedCases(String id, boolean exportContacts, boolean showCompany, boolean addDefaultFields);
 	public boolean setCasesToExport(String id, List<CasePresentation> cases);
 
 	public boolean clearSearchResults(String id);
